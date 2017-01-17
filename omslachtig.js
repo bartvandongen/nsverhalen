dafd388deef80d4feb81ec40d816b941ea3ea2d3
bartvandongen <vandongen.bart@gmail.com>
2017-01-17 21:00:15 +0100

    verhalen op home toegevoegd, droom tekst animations

--- omslachtig.js
+++ omslachtig.js
@@ -1,9 +1,12 @@
-var button1 = document.querySelector('button');
-
-var eersteStap = document.querySelector('.tweede_paragraaf');
-
-var visibleMaken = function () {
-    eersteStap.classList.toggle('expanded');
+var invisibleMaken = function () {              document.querySelector('img[src="images/enigzins.svg"]').classList.add('noshow');
+ document.querySelector('.button1').classList.add('noshow');
 };
 
-button1.addEventListener('click', visibleMaken);
+document.querySelector('.button1').addEventListener('click', invisibleMaken);
+
+var visibleMaken = function () {      
+ document.querySelector('.stap1').classList.add('expanded');
+ document.querySelector('.button2').classList.add('noshow');
+};
+
+document.querySelector('.button2').addEventListener('click', visibleMaken);
