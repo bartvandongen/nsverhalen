347eb77167e69013ed9f430033adcacc946b8207
bartvandongen <vandongen.bart@gmail.com>
2017-02-01 16:39:03 +0100

    filter bar

---
+++ script.js
@@ -0,0 +1,23 @@
+var dropdown = function () {              
+    
+    document.querySelector('.leestijd_noshow').classList.toggle('drop');
+    
+};
+
+document.querySelector('.leestijd').addEventListener('click', dropdown);
+
+var dropdown = function () {              
+    
+    document.querySelector('.sorteren_noshow').classList.toggle('drop');
+    
+};
+
+document.querySelector('.sorteren').addEventListener('click', dropdown);
+
+var dropdown = function () {              
+    
+    document.querySelector('.sfeerwoord_noshow').classList.toggle('drop');
+    
+};
+
+document.querySelector('.sfeerwoord').addEventListener('click', dropdown);
